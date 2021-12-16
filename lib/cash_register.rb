4b5cd7417db6a554ec7e8eac349bf37eec8616a4
Nicholas Shephard <shephardn@gmail.com>
2021-12-16 10:20:41 -0600

    comm

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,20 @@
 
 class CashRegister
 
-    attr_accessor :total
+    attr_accessor :total, :discount
 
-    def initialize(discount=20)
+    def initialize(discount=0)
         @total = 0
         @discount = discount
+    end
+
+    def add_item(title, price, quantity = 1)
+        if discount == 0 
+            self.total = total + (quantity * price)
+        else
+            self.total = total + (quantity * price * discount / 100)
+        # self.total = total + (quantity * price) * discount
+        end
     end
 
 end
